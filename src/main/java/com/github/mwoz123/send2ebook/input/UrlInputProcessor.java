f2a3b5be7ad2b129035ff4bb9f578c70c690b784
mwoz123 <mwoz123@github.com>
2018-02-13 23:00:28 +0100

    Image cache, download img only once

--- src/main/java/com/github/mwoz123/send2ebook/input/UrlInputProcessor.java
+++ src/main/java/com/github/mwoz123/send2ebook/input/UrlInputProcessor.java
@@ -2,6 +2,8 @@ package com.github.mwoz123.send2ebook.input;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
@@ -15,34 +17,35 @@ import org.jsoup.select.Elements;
 import com.github.mwoz123.send2ebook.model.EbookData;
 import com.github.mwoz123.send2ebook.util.TitleExtractor;
 
-
 public class UrlInputProcessor implements InputProcessor<String> {
 
-	private static final Logger LOGGER = LogManager.getLogger(UrlInputProcessor.class);
+	private static final Logger LOGGER = LogManager
+			.getLogger(UrlInputProcessor.class);
 
 	private final static Whitelist CLEAN_UP_LEVEL = Whitelist.basicWithImages();
 
 	private final static String HTML_EXTENSION = ".html";
 	private final static String IMG_SRC_ATTR = "src";
 
-	public EbookData transformInput(String urlString, boolean processTextOnly) throws IOException {
+	public EbookData transformInput(String urlString, boolean processTextOnly)
+			throws IOException {
 
 		Document oryginalDoc = Jsoup.connect(urlString).get();
 		Document cleanedUpDoc = this.cleanDocument(oryginalDoc);
 
 		String titleFromUrl = TitleExtractor.extractTitleFromUrl(urlString);
-		
+
 		EbookData ebookData = new EbookData();
 		ebookData.setTitle(titleFromUrl);
 
-		if (! processTextOnly) { 
-			cleanedUpDoc = this.addImages(cleanedUpDoc, ebookData);
-			//TODO add CSS styles?
+		if (!processTextOnly) {
+			this.addImages(cleanedUpDoc, ebookData);
+			// TODO add CSS styles?
 		}
 		String cleanedUpDocHtml = cleanedUpDoc.html();
 		ebookData.addElement(titleFromUrl + HTML_EXTENSION,
 				cleanedUpDocHtml.getBytes());
-		
+
 		return ebookData;
 	}
 
@@ -59,35 +62,51 @@ public class UrlInputProcessor implements InputProcessor<String> {
 		return cleanedUpDoc;
 	}
 
-	private Document addImages(Document cleanedUpDoc, EbookData ebookData) {
+	private void addImages(Document cleanedUpDoc, EbookData ebookData) {
 		LOGGER.debug("Processing images");
 		Elements images = cleanedUpDoc.getElementsByTag("img");
-		
-		
+		Map<String, String> allreadyProcessedMap = new HashMap<>();
+
 		for (int i = 0; i < images.size(); i++) {
 			Element img = images.get(i);
 			// TODO refactor to threads
-			
+
 			String imgSrc = img.attr(IMG_SRC_ATTR);
-			LOGGER.debug("Processing image: {}", imgSrc);
-			//TODO add check if allready processed if yes store, new name in HashSet
-			try {
-				URL url = new URL(imgSrc);
-				byte[] imageBytes = IOUtils.toByteArray(url.openStream());
-
-				String extension = TitleExtractor.getExtension(imgSrc);
-				String imageFilename = new StringBuffer(Integer.toString(i)).append(".")
-						.append(extension).toString();
-
-				ebookData.addElement(imageFilename, imageBytes);
-				
-				img.attr(IMG_SRC_ATTR, imageFilename);
-
-			} catch (IOException ex) {
-				LOGGER.error("Couldn't save image ", ex);
+			if (allreadyProcessedMap.containsKey(imgSrc)) {
+				this.getNewImgSrcFromAllreadyProcessed(allreadyProcessedMap, img, imgSrc);
+			} else {
+				this.downloadImgAddToEbookDataAndAddToAlreadyProcessed(ebookData,
+						allreadyProcessedMap, i, img, imgSrc);
 			}
 		}
-		return cleanedUpDoc;
+	}
+
+	private void getNewImgSrcFromAllreadyProcessed(Map<String, String> allreadyProcessedSet,
+			Element img, String imgSrc) {
+		String newImgSrc = allreadyProcessedSet.get(imgSrc);
+		img.attr(IMG_SRC_ATTR, newImgSrc);
+		LOGGER.debug("Already downloaded img {} redirecting to cache {}", imgSrc, newImgSrc);
+	}
+
+	private void downloadImgAddToEbookDataAndAddToAlreadyProcessed(
+			EbookData ebookData, Map<String, String> allreadyProcessedSet,
+			int i, Element img, String imgSrc) {
+		LOGGER.debug("Processing image: {}", imgSrc);
+		try {
+			URL url = new URL(imgSrc);
+			byte[] imageBytes = IOUtils.toByteArray(url.openStream());
+
+			String extension = TitleExtractor.getExtension(imgSrc);
+			String imgNewSrc = new StringBuffer(Integer.toString(i))
+					.append(".").append(extension).toString();
+
+			ebookData.addElement(imgNewSrc, imageBytes);
+
+			img.attr(IMG_SRC_ATTR, imgNewSrc);
+			allreadyProcessedSet.put(imgSrc, imgNewSrc);
+		} catch (IOException ex) {
+			LOGGER.error("Couldn't save image ", ex);
+		}
 	}
 
 }
