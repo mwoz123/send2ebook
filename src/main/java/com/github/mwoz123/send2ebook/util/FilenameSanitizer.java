22b7329be525da0cb97efb21a997b71c331d6ac8
mwoz123 <mwoz123@github.com>
2018-02-28 20:03:41 +0100

    Sanitarize more chars

--- src/main/java/com/github/mwoz123/send2ebook/util/FilenameSanitizer.java
+++ src/main/java/com/github/mwoz123/send2ebook/util/FilenameSanitizer.java
@@ -2,7 +2,7 @@ package com.github.mwoz123.send2ebook.util;
 
 public class FilenameSanitizer {
 
-	private final static String INVALID_CHAR_REGEX = "[:\\\\/*?|<>#]",
+	private final static String INVALID_CHAR_REGEX = "[^a-zA-Z0-9.+ -]+",
 			SUBSTITUTE = "_";
 
 	public static String replaceInvalidChars(String title) {
